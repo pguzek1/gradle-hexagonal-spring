60c1525356681c7cf7d4054c6d83872c5a9d93ce
Paweł Guzek <pawelguzek8@gmail.com>
2023-06-08 14:11:15 +0200

    chore(gradle): update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,179 +1,147 @@
-import org.gradle.api.JavaVersion.VERSION_17
+import java.lang.Runtime.getRuntime
+import org.gradle.accessors.dm.LibrariesForCommonLibs
+import org.gradle.accessors.dm.LibrariesForGeneral
+import org.gradle.accessors.dm.LibrariesForMicronautLibs
+import org.gradle.accessors.dm.LibrariesForSpringLibs
+import org.gradle.accessors.dm.LibrariesForWiremockLibs
 import org.gradle.api.attributes.TestSuiteType.INTEGRATION_TEST
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
 import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR
-import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.7.10" apply false
-    id("org.springframework.boot") version "2.7.2" apply false
-    id("pl.allegro.tech.build.axion-release") version "1.14.0"
-    id("jvm-test-suite")
-    id("idea")
+    alias(commonLibs.plugins.axion)
+    alias(commonLibs.plugins.kotlinJvm)
+
+    alias(springLibs.plugins.springKotlin) apply false
+    alias(springLibs.plugins.spring) apply false
+
+    alias(micronautLibs.plugins.micronaut) apply false
+
+    `jvm-test-suite`
 }
 
-allprojects {
-    apply(plugin = "org.jetbrains.kotlin.jvm")
-    apply(plugin = "pl.allegro.tech.build.axion-release")
-    apply(plugin = "jvm-test-suite")
-    apply(plugin = "idea")
+// project configuration
+subprojects {
+    apply(plugin = commonLibs.plugins.kotlinJvm.get().pluginId)
+    apply(plugin = commonLibs.plugins.axion.get().pluginId)
 
-    group = "org.example.project"
+    group = "pl.alfn.project"
     version = scmVersion.version
 
-    ext.apply {
-        set("javaVersion", VERSION_17)
-        set("kotlinVersion", "1.7.10")
-
-        set("kotlinxVersion", "1.6.4")
-        set("kotlinLoggingVersion", "2.1.23")
-
-        set("kotestVersion", "5.4.1")
-        set("kotestSpringVersion", "1.1.2")
-        set("mockkVersion", "1.12.5")
-        set("mockkSpringVersion", "3.1.1")
-
-        set("reactorVersion", "3.4.22")
-        set("reactorKotlinVersion", "1.1.7")
-
-        set("springBootVersion", "2.7.2")
-        set("springFrameworkVersion", "5.3.22")
-//        set("springSecurityVersion", "5.7.2")
-
-        set("jacksonVersion", "2.13.3")
-    }
-
-    repositories {
-        mavenCentral()
-    }
-
-    configure<JavaPluginExtension> {
-        sourceCompatibility = project.ext["javaVersion"] as JavaVersion
-        targetCompatibility = project.ext["javaVersion"] as JavaVersion
-    }
-
-    tasks.withType<Test> {
-        useJUnitPlatform()
-
-        testLogging {
-            exceptionFormat = FULL
-            showExceptions = true
-            showStandardStreams = true
-            events(PASSED, FAILED, SKIPPED, STANDARD_OUT, STANDARD_ERROR)
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(javaVersion))
         }
     }
 
-    tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = (project.ext["javaVersion"] as JavaVersion).majorVersion
+    tasks {
+        withType<JavaCompile> {
+            options.compilerArgs = listOf("-Xlint:all", "-parameters")
+        }
+
+        withType<KotlinCompile> {
+            compilerOptions {
+                apiVersion.set(kotlinVersion)
+                freeCompilerArgs.set(listOf("-Xjsr305=strict"))
+                languageVersion.set(kotlinVersion)
+                useK2.set(false) // FIXME
+            }
         }
     }
+}
 
-    testing {
-        suites {
-            @Suppress("UnstableApiUsage")
-            val testIntegration by registering(JvmTestSuite::class) {
-                val taskName = "testIntegration"
-                testType.set(INTEGRATION_TEST)
+// test configuration
+subprojects {
+    apply(plugin = "jvm-test-suite")
 
-                dependencies {
-                    implementation(project)
-                }
+    tasks {
+        withType<Test> {
+            useJUnitPlatform()
+            maxParallelForks = 1.coerceAtLeast(getRuntime().availableProcessors() / 2)
 
-                sources {
-                    compileClasspath += sourceSets.test.get().output
-                    runtimeClasspath += sourceSets.test.get().output
-                }
+            testLogging {
+                exceptionFormat = FULL
+                events(FAILED, SKIPPED)
+            }
+        }
 
-                configurations {
-                    get(sources.implementationConfigurationName).extendsFrom(configurations.testImplementation.get())
-                    get(sources.compileOnlyConfigurationName).extendsFrom(configurations.testCompileOnly.get())
-                    get(sources.runtimeOnlyConfigurationName).extendsFrom(configurations.testRuntimeOnly.get())
-                }
+        testing {
+            suites {
+                register<JvmTestSuite>("testIntegration") {
+                    testType.set(INTEGRATION_TEST)
 
-                targets {
-                    all {
-                        testTask.configure {
-                            testClassesDirs += sourceSets[taskName].output.classesDirs
+                    sources {
+                        compileClasspath += sourceSets.test.get().output
+                        runtimeClasspath += sourceSets.test.get().output
+                    }
 
-                            classpath += sourceSets[taskName].runtimeClasspath
-                            classpath += sourceSets[taskName].output
+                    configurations {
+                        get(sources.implementationConfigurationName).extendsFrom(testImplementation.get())
+                        get(sources.compileOnlyConfigurationName).extendsFrom(testCompileOnly.get())
+                        get(sources.runtimeOnlyConfigurationName).extendsFrom(testRuntimeOnly.get())
+                    }
 
-                            shouldRunAfter(tasks.test)
+                    targets {
+                        all {
+                            testTask.configure {
+                                testClassesDirs += sourceSets[this@register.name].output.classesDirs
+
+                                classpath += sourceSets[this@register.name].runtimeClasspath
+                                classpath += sourceSets[this@register.name].output
+
+                                shouldRunAfter(this@tasks.test)
+                            }
                         }
                     }
-                }
 
-                tasks.check {
-                    dependsOn(tasks.getByName(taskName))
-                }
-
-                idea {
-                    module {
-                        testSourceDirs.addAll(sources.java.srcDirs)
-                        testResourceDirs.addAll(sources.resources.srcDirs)
+                    this@tasks.check {
+                        dependsOn(this@register)
                     }
                 }
             }
         }
     }
-
-    dependencies {
-        implementation(group = "org.jetbrains.kotlin", name = "kotlin-reflect", version = "${project.ext["kotlinVersion"]}")
-        implementation(group = "org.jetbrains.kotlin", name = "kotlin-stdlib-jdk8", version = "${project.ext["kotlinVersion"]}")
-
-        implementation(group = "io.github.microutils", name = "kotlin-logging", version = "${project.ext["kotlinLoggingVersion"]}")
-
-        // ---
-
-        testImplementation(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-debug", version = "${project.ext["kotlinxVersion"]}")
-
-        testImplementation(group = "io.kotest", name = "kotest-runner-junit5", version = "${project.ext["kotestVersion"]}")
-        testImplementation(group = "io.kotest", name = "kotest-assertions-core", version = "${project.ext["kotestVersion"]}")
-        testImplementation(group = "io.kotest", name = "kotest-property", version = "${project.ext["kotestVersion"]}")
-
-        testImplementation(group = "io.mockk", name = "mockk", version = "${project.ext["mockkVersion"]}")
-    }
 }
 
+// dependencies configuration
 subprojects {
-    if (name.contains("spring")) {
-        if (name.contains("runnable")) apply(plugin = "org.springframework.boot")
-        apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    dependencies {
+        runtimeOnly(commonLibs.bundles.logback.runtime)
+        implementation(commonLibs.bundles.kotlin.impl)
+        testImplementation(commonLibs.bundles.kotest.test)
+    }
 
+    if (name.contains("spring")) {
+        apply(plugin = springLibs.plugins.springKotlin.get().pluginId)
+        if (name.contains("run")) {
+            apply(plugin = springLibs.plugins.spring.get().pluginId)
+        }
 
         dependencies {
-//            implementation(group = "org.springframework", name = "spring-tx", version = "${project.ext["springFrameworkVersion"]}")
-//            implementation(group = "org.springframework", name = "spring-webflux", version = "${project.ext["springFrameworkVersion"]}")
-//            implementation(group = "org.springframework.security", name = "spring-security-core", version = "${project.ext["springSecurityVersion"]}")
+            val testIntegrationImplementation by configurations
+            testIntegrationImplementation(springLibs.bundles.kotest.spring)
+        }
+    }
 
-            //FIXME: testIntegration handler is not registered in root file (workaround)
-            "testIntegrationImplementation"(group = "org.springframework.boot", name= "spring-boot-starter-test", version = "${project.ext["springBootVersion"]}")
-            "testIntegrationImplementation"(group = "io.kotest.extensions", name = "kotest-extensions-spring", version = "${project.ext["kotestSpringVersion"]}")
-            "testIntegrationImplementation"(group = "com.ninja-squad", name = "springmockk", version = "${project.ext["mockkSpringVersion"]}")
+    if (name.contains("micronaut")) {
+        apply(plugin = micronautLibs.plugins.kotlin.kapt.get().pluginId)
+        apply(plugin = micronautLibs.plugins.kotlin.allOpen.get().pluginId)
+        if (name.contains("run")) {
+            apply(plugin = micronautLibs.plugins.micronaut.get().pluginId)
         }
     }
 }
 
-tasks.wrapper {
-    gradleVersion = "7.5.1"
-}
-
-//FIXME: exclude rootProject build directory (alternative will be available in gradle 7.6)
-tasks.forEach {
-    if (it.name !in listOf("wrapper")) {
-        it.enabled = false
-    }
-}
-gradle.buildFinished {
-    buildDir.deleteRecursively()
-}
+val Project.general get() = rootProject.extensions.getByName("general") as LibrariesForGeneral
+val Project.javaVersion: String get() = rootProject.general.versions.java.get()
+val Project.kotlinVersion: KotlinVersion get() = KotlinVersion.fromVersion(rootProject.general.versions.kotlin.get().substringBeforeLast('.'))
+val Project.commonLibs get() = rootProject.extensions.getByName("commonLibs") as LibrariesForCommonLibs
+val Project.springLibs get() = rootProject.extensions.getByName("springLibs") as LibrariesForSpringLibs
+val Project.wiremockLibs get() = rootProject.extensions.getByName("wiremockLibs") as LibrariesForWiremockLibs
+val Project.micronautLibs get() = rootProject.extensions.getByName("micronautLibs") as LibrariesForMicronautLibs
 
 // Foldable ProjectView
-//.gitignore gradlew gradlew.bat gradle .idea .git
+// .gitignore gradlew gradlew.bat gradle .idea .git
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,132 @@
-rootProject.name = "hexagonal"
+import org.gradle.api.initialization.resolve.RepositoriesMode.FAIL_ON_PROJECT_REPOS
 
-// + -------------- +
-// |    runnable    |
-// + -------------- +
-include(":runnable-spring")
-project(":runnable-spring").projectDir = file("runnable-spring")
-project(":runnable-spring").name = "runnable-spring"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+    }
+
+    versionCatalogs {
+        val kotlinVersion = "1.8.21"
+
+        create("general") {
+            version("java", "17")
+            version("kotlin", kotlinVersion)
+        }
+
+        create("commonLibs") {
+            version("kotlin", kotlinVersion)
+            version("axion", "1.15.3")
+
+            version("kotlin-logging", "3.0.5")
+            version("logback", "1.4.7")
+            version("fasterxml-uuids", "4.2.0")
+
+            version("kotest", "5.6.2")
+            version("kotlinx", "1.7.1")
+            version("mockk", "1.13.5")
+
+            // plugin
+            plugin("kotlinJvm", "org.jetbrains.kotlin.jvm").versionRef("kotlin")
+            plugin("axion", "pl.allegro.tech.build.axion-release").versionRef("axion")
+
+            // runtime
+            library("logback", "ch.qos.logback", "logback-classic").versionRef("logback")
+
+            // implementation
+            library("kotlin-reflect", "org.jetbrains.kotlin", "kotlin-reflect").versionRef("kotlin")
+            library("kotlin-std", "org.jetbrains.kotlin", "kotlin-stdlib").versionRef("kotlin")
+            library("kotlin-logging", "io.github.microutils", "kotlin-logging").versionRef("kotlin-logging")
+            library("fasterxml-uuids", "com.fasterxml.uuid", "java-uuid-generator").versionRef("fasterxml-uuids")
+
+            // test
+            library("kotest-runner", "io.kotest", "kotest-runner-junit5").versionRef("kotest")
+            library("kotest-assertions", "io.kotest", "kotest-assertions-core").versionRef("kotest")
+            library("kotest-property", "io.kotest", "kotest-property").versionRef("kotest")
+            library("kotlinx-coroutines-dbg", "org.jetbrains.kotlinx", "kotlinx-coroutines-debug").versionRef("kotlinx")
+            library("mockk", "io.mockk", "mockk").versionRef("mockk")
+
+            // bundle
+            bundle("logback-runtime", listOf("logback"))
+            bundle("kotlin-impl", listOf("kotlin-reflect", "kotlin-std", "kotlin-logging", "fasterxml-uuids"))
+            bundle("kotest-test", listOf("kotest-runner", "kotest-assertions", "kotest-property", "kotlinx-coroutines-dbg", "mockk"))
+        }
+
+        create("springLibs") {
+            version("kotlin", kotlinVersion)
+            version("spring-boot", "3.1.0")
+
+            version("kotest-spring", "1.1.3")
+            version("mockk-spring", "4.0.2")
+
+            // plugin
+            plugin("springKotlin", "org.jetbrains.kotlin.plugin.spring").versionRef("kotlin")
+            plugin("spring", "org.springframework.boot").versionRef("spring-boot")
+
+            // implementation
+            library("spring-starter", "org.springframework.boot", "spring-boot-starter").versionRef("spring-boot")
+
+            // testIntegration
+            library("spring-test", "org.springframework.boot", "spring-boot-starter-test").versionRef("spring-boot")
+            library("kotest-spring", "io.kotest.extensions", "kotest-extensions-spring").versionRef("kotest-spring")
+            library("mockk-spring", "com.ninja-squad", "springmockk").versionRef("mockk-spring")
+
+            // bundle
+            bundle("kotest-spring", listOf("spring-test", "kotest-spring", "mockk-spring"))
+        }
+
+        create("wiremockLibs") {
+            version("wiremock", "3.0.0-beta-8")
+            version("kotest-wiremock", "1.0.3")
+
+            // testIntegration
+            library("wiremock", "com.github.tomakehurst", "wiremock").versionRef("wiremock")
+            library("kotest-wiremock", "io.kotest.extensions", "kotest-extensions-wiremock").versionRef("kotest-wiremock")
+
+            // bundle
+            bundle("kotest-wiremock", listOf("wiremock", "kotest-wiremock"))
+        }
+
+        create("micronautLibs") {
+            version("kotlin", kotlinVersion)
+            version("micronaut-plugin", "3.7.10")
+
+            version("micronaut-general", "3.9.3")
+
+            // plugin
+            plugin("micronaut", "io.micronaut.application").versionRef("micronaut-plugin")
+            plugin("kotlin-kapt", "org.jetbrains.kotlin.kapt").versionRef("kotlin")
+            plugin("kotlin-allOpen", "org.jetbrains.kotlin.plugin.allopen").versionRef("kotlin")
+
+            // kapt
+            library("micronaut-httpValidation", "io.micronaut", "micronaut-http-validation").versionRef("micronaut-general")
+
+            // runtime
+            library("jackson-module-kotlin", "com.fasterxml.jackson.module", "jackson-module-kotlin").withoutVersion()
+
+            // implementation
+            library("micronaut-httpClient", "io.micronaut", "micronaut-http-client").versionRef("micronaut-general")
+            library("micronaut-jacksonDatabind", "io.micronaut", "micronaut-jackson-databind").versionRef("micronaut-general")
+            library("micronaut-validation", "io.micronaut", "micronaut-validation").versionRef("micronaut-general")
+            library("micronaut-runtime", "io.micronaut.kotlin", "micronaut-kotlin-runtime").withoutVersion()
+            library("jakarta-annotation", "jakarta.annotation", "jakarta.annotation-api").withoutVersion()
+
+            // bundle
+            bundle("micronaut-general", listOf("micronaut-httpClient", "micronaut-jacksonDatabind", "micronaut-validation", "micronaut-runtime", "jakarta-annotation"))
+            bundle("micronaut-kapt", listOf("micronaut-httpValidation"))
+            bundle("micronaut-runtime", listOf("jackson-module-kotlin"))
+        }
+    }
+}
+
+
+rootProject.name = "hexagonal"
 
 
 // + -------------- +
@@ -23,19 +144,42 @@ project(":application-core-services").name = "core-services"
 // + -------------- +
 // |     ports      |
 // + -------------- +
-//TODO: split ports-driven, ports-driving, ports-model
-include(":application-ports")
-project(":application-ports").projectDir = file("application/ports")
-project(":application-ports").name = "application-ports"
+include(":application-ports-model")
+project(":application-ports-model").projectDir = file("application/ports/model")
+project(":application-ports-model").name = "ports-model"
+
+include(":application-ports-driven")
+project(":application-ports-driven").projectDir = file("application/ports/driven")
+project(":application-ports-driven").name = "ports-driven"
+
+include(":application-ports-driving")
+project(":application-ports-driving").projectDir = file("application/ports/driving")
+project(":application-ports-driving").name = "ports-driving"
 
 
 // + -------------- +
-// |    adapter     |
+// |    adapters    |
 // + -------------- +
-include(":adapter-spring-rest-reactive")
-project(":adapter-spring-rest-reactive").projectDir = file("application/adapter/spring-rest-reactive")
-project(":adapter-spring-rest-reactive").name = "adapter-spring-rest-reactive"
+include(":application-adapter-spring-persistence-inmemory")
+project(":application-adapter-spring-persistence-inmemory").projectDir = file("application/adapters/spring-persistence-inmemory")
+project(":application-adapter-spring-persistence-inmemory").name = "spring-persistence-inmemory"
 
-include(":adapter-memory-persistence-store")
-project(":adapter-memory-persistence-store").projectDir = file("application/adapter/memory-persistence-store")
-project(":adapter-memory-persistence-store").name = "adapter-memory-persistence-store"
+include(":application-adapter-spring-persistence-scylla")
+project(":application-adapter-spring-persistence-scylla").projectDir = file("application/adapters/spring-persistence-scylla")
+project(":application-adapter-spring-persistence-scylla").name = "spring-persistence-scylla"
+
+include(":application-adapter-spring-rest-reactive")
+project(":application-adapter-spring-rest-reactive").projectDir = file("application/adapters/spring-rest-reactive")
+project(":application-adapter-spring-rest-reactive").name = "spring-rest-reactive"
+
+
+// + -------------- +
+// |    runnable    |
+// + -------------- +
+include(":spring-runnable")
+project(":spring-runnable").projectDir = file("runnable/spring-runnable")
+project(":spring-runnable").name = "spring-runnable"
+
+include(":micronaut-runnable")
+project(":micronaut-runnable").projectDir = file("runnable/micronaut-runnable")
+project(":micronaut-runnable").name = "micronaut-runnable"
